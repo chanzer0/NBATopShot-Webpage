c2dc53bdf174fd8254df72f3293b9f7788539772
Sean Sailer <seansailer28@gmail.com>
2021-03-04 11:15:38 -0600

    Update contact.js

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,10 +4,6 @@ class Contact extends Component {
     render() {
         return (
             <div>
-                <h2>GOT QUESTIONS?</h2>
-                <p>The easiest thing to do is post on
-        our <a href="http://forum.kirupa.com">forums</a>.
-        </p>
             </div>
         );
     }
